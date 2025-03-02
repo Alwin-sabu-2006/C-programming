e3630f1ebec65e2aaa70b0e0f98a594cb57d9d7a
Alwin Sabu <alwinsabu2028@cs.sjcetpalai.ac.in>
2025-03-02 15:05:05 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{    
+char c;    
+printf("Enter the character whose ASCII value is to be printed");
+scanf("%c",&c);    
+printf("\n The ASCII value of %c is: %d",c,c);    
+return 0;
+}
